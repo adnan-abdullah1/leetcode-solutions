b73716c0ce6a2a890195ffde2a4e6e3349ed0e9a
Adnan <adnan.abdullah@demandscience.com>
2025-11-15 21:16:04 +0530

    IsPermOfPallindrome.go

---
+++ IsPermOfPallindrome.go
@@ -0,0 +1,29 @@
+package main
+
+func IsPermOfPallindrome(str string) bool{
+	table := [26]int{}
+	oddCount:=0
+	for _,v:=range str {
+		if v==' '{
+			continue
+		}
+
+		if v >= 'A' && v<='Z'{
+			v = v + ('a'-'A')
+		}
+
+		if v < 'a' || v > 'z'{
+			continue
+		}
+
+		table[v-'a']++
+		if table[v-'a'] % 2 != 0 {
+			oddCount++
+		} else{
+			oddCount--
+		}
+
+	}
+
+	return oddCount <= 1
+}

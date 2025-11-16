f8322681a4bc69f7b9c5967e0a6f4398722e8116
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-11-16 18:30:02 +0530

    Time: 0 ms (100%), Space: 4.1 MB (68.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.go
+++ 0027-remove-element/0027-remove-element.go
@@ -1,38 +1,27 @@
-// func removeElement(nums []int, val int) int {
-// 	// [5,6,3,3,,3,1,3,8,7]
-//     k:=1
-//     j := 0
-// 	for i := 0; i < len(nums); i++ {
-//         if nums[i] != val{
-//             continue
-//         }
-
-//         for j<len(nums) && val == nums[j]{
-//             j++
-//         }
-
-//         if j >= len(nums){
-//             break;
-//         }
-
-//         nums[i]=nums[j];
-//         k++
-// 	}
-//     return k
-// }
 
 // with extra mem
+// func removeElement(nums []int, val int) int {
+// 	ans := []int{}
+
+// 	for _, v := range nums {
+
+// 		if v != val {
+// 			ans = append(ans, v)
+// 		}
+// 	}
+
+// 	copy(nums, ans)
+// 	return len(ans)
+
+// }
+
 func removeElement(nums []int, val int) int {
-	ans := []int{}
-
+	k := 0
 	for _, v := range nums {
-
 		if v != val {
-			ans = append(ans, v)
+			nums[k] = v
+			k++
 		}
 	}
-
-	copy(nums, ans)
-	return len(ans)
-
+	return k
 }

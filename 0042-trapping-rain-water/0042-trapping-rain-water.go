93994d74a52803154521e98796446d649b5e3b19
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-11-19 23:35:12 +0530

    Time: 19 ms (12.48%), Space: 8.6 MB (6.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.go
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -1,14 +1,13 @@
 // n square solution
-func trap(arr []int) int {
-    trapped := 0
-	for i := range arr {
-		rMax := getRightMax(arr, i)
-		lMax := getLeftMax(arr, i)
-		trapped += min(rMax, lMax) - arr[i]
-	}
-	return trapped
-}
-
+// func trap(arr []int) int {
+//     trapped := 0
+// 	for i := range arr {
+// 		rMax := getRightMax(arr, i)
+// 		lMax := getLeftMax(arr, i)
+// 		trapped += min(rMax, lMax) - arr[i]
+// 	}
+// 	return trapped
+// }
 
 func getLeftMax(arr []int, i int) int {
 	maxNo := 0
@@ -30,8 +29,45 @@ func getRightMax(arr []int, i int) int {
 	return maxNo
 }
 
-// better solution can be by computing 
+// better solution can be by computing
 // premax sum, and postmax sum for every element
 // tc: o(n), sc: o(n)
+
+func trap(arr []int) int {
+	maxLeft := calcPreMax(arr)
+	maxRight := calcPostMax(arr)
+	fmt.Println(maxLeft)
+	fmt.Println(maxRight)
+
+	trapped := 0
+	for i := range arr {
+		trapped += min(maxLeft[i], maxRight[i]) - arr[i]
+	}
+	return trapped
+}
+
+func calcPreMax(arr []int) []int {
+	maxNo := 0
+	res := make([]int, len(arr))
+	for i, v := range arr {
+		if v > maxNo {
+			maxNo = v
+		}
+		res[i] = maxNo
+	}
+	return res
+}
+func calcPostMax(arr []int) []int {
+	maxNo := 0
+	res := make([]int, len(arr))
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		if arr[i] > maxNo {
+			maxNo = arr[i]
+		}
+		res[i] = maxNo
+	}
+	return res
+}
 
 // most optimal soution is two pointer

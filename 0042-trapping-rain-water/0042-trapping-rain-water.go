80ea14717914cb12cf4a3324b05d65357cd40517
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-11-19 23:35:39 +0530

    Time: 723 ms (5.08%), Space: 7.7 MB (95.99%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.go
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -1,13 +1,13 @@
 // n square solution
-// func trap(arr []int) int {
-//     trapped := 0
-// 	for i := range arr {
-// 		rMax := getRightMax(arr, i)
-// 		lMax := getLeftMax(arr, i)
-// 		trapped += min(rMax, lMax) - arr[i]
-// 	}
-// 	return trapped
-// }
+func trap(arr []int) int {
+    trapped := 0
+	for i := range arr {
+		rMax := getRightMax(arr, i)
+		lMax := getLeftMax(arr, i)
+		trapped += min(rMax, lMax) - arr[i]
+	}
+	return trapped
+}
 
 func getLeftMax(arr []int, i int) int {
 	maxNo := 0
@@ -33,18 +33,18 @@ func getRightMax(arr []int, i int) int {
 // premax sum, and postmax sum for every element
 // tc: o(n), sc: o(n)
 
-func trap(arr []int) int {
-	maxLeft := calcPreMax(arr)
-	maxRight := calcPostMax(arr)
-	fmt.Println(maxLeft)
-	fmt.Println(maxRight)
+// func trap(arr []int) int {
+// 	maxLeft := calcPreMax(arr)
+// 	maxRight := calcPostMax(arr)
+// 	fmt.Println(maxLeft)
+// 	fmt.Println(maxRight)
 
-	trapped := 0
-	for i := range arr {
-		trapped += min(maxLeft[i], maxRight[i]) - arr[i]
-	}
-	return trapped
-}
+// 	trapped := 0
+// 	for i := range arr {
+// 		trapped += min(maxLeft[i], maxRight[i]) - arr[i]
+// 	}
+// 	return trapped
+// }
 
 func calcPreMax(arr []int) []int {
 	maxNo := 0

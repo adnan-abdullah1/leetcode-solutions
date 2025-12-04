21951d1ba910cce046b514d74b019a1488a184e3
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-04 23:20:16 +0530

    Time: 0 ms (100%), Space: 6 MB (82.19%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.go
@@ -0,0 +1,73 @@
+// func evalRPN(tokens []string) int {
+//     stack := []int{}
+
+//     for _, v := range tokens {
+//         if v == "*" || v == "/" || v == "-" || v == "+" {
+//             stack, op1 := pop(stack)
+//             stack, op2 := pop(stack)
+
+//             res := 0
+//             if v == "+" {
+//                 res = op2 + op1
+//             } else if v == "-" {
+//                 res = op2 - op1
+//             } else if v == "*" {
+//                 res = op2 * op1        // FIXED
+//             } else {
+//                 res = op2 / op1
+//             }
+
+//             stack = append(stack, res)
+//         } else {
+//             val, _ := strconv.Atoi(v)
+//             stack = append(stack, val)
+//         }
+//     }
+
+//     return stack[len(stack)-1]
+// }
+
+// func pop(stack []int) ([]int, int) {
+//     el := stack[len(stack)-1]
+//     return stack[:len(stack)-1], el
+// }
+
+
+func evalRPN(tokens []string) int {
+    stack := []int{}
+
+    for _, v := range tokens {
+        if v == "*" || v == "/" || v == "-" || v == "+" {
+
+            var op1, op2 int
+            stack, op1 = pop(stack)
+            stack, op2 = pop(stack)
+
+            res := 0
+
+            if v == "+" {
+                res = op2 + op1
+            } else if v == "-" {
+                res = op2 - op1
+            } else if v == "*" {
+                res = op2 * op1
+            } else {
+                res = op2 / op1
+            }
+
+            stack = append(stack, res)
+
+        } else {
+            // safe
+            val, _ := strconv.Atoi(v)
+            stack = append(stack, val)
+        }
+    }
+
+    return stack[len(stack)-1]
+}
+
+func pop(stack []int) ([]int, int) {
+    el := stack[len(stack)-1]
+    return stack[:len(stack)-1], el
+}

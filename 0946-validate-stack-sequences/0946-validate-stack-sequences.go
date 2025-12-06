13780a8b34912cea58c9ff449ff337c176f79d8a
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-06 21:21:50 +0530

    Time: 0 ms (100%), Space: 5.4 MB (28.89%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.go
@@ -0,0 +1,15 @@
+func validateStackSequences(pushed []int, popped []int) bool {
+    stack := []int{}
+    j := 0
+
+    for _, v := range pushed {
+        stack = append(stack, v)
+
+        for len(stack) > 0 && stack[len(stack)-1] == popped[j] {
+            stack = stack[:len(stack)-1]
+            j++
+        }
+    }
+
+    return j == len(popped)
+}

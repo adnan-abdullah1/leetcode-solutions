ac6bf775798ba24417c1699e03e281c4485c8cf3
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-07 11:58:34 +0530

    Time: 2 ms (15%), Space: 5.2 MB (49.41%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.go
@@ -0,0 +1,29 @@
+func nextGreaterElement(nums1 []int, arr []int) []int {
+    stack := []int{}
+    mp := make(map[int]int, len(arr))
+
+    mp[arr[len(arr)-1]] = -1
+    stack = append(stack, arr[len(arr)-1])
+
+    for i := len(arr) - 2; i >= 0; i-- {
+        for len(stack) > 0 && arr[i] >= stack[len(stack)-1] {
+            stack = stack[:len(stack)-1]
+        }
+
+        if len(stack) == 0 {
+            mp[arr[i]] = -1
+        } else {
+            mp[arr[i]] = stack[len(stack)-1]   
+        }
+
+        stack = append(stack, arr[i])
+    }
+
+    ans := []int{}
+    for _, v := range nums1 {                  
+        ans = append(ans, mp[v])
+    }
+
+    fmt.Println(ans)
+    return ans
+}

9d722c132bb1eb3174590090ca2b79af3b3b394f
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-07 13:23:25 +0530

    Time: 1 ms (55.28%), Space: 9.1 MB (23.6%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.go
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.go
@@ -1,5 +1,5 @@
 // using virtaul duplicate array (striver)
-func nextGreaterElements(nums []int) []int {
+func BruteNextGreaterElements(nums []int) []int {
 	n := len(nums)
 	nge := make([]int, n)
 
@@ -8,7 +8,7 @@ func nextGreaterElements(nums []int) []int {
 	}
 
 	for i := 0; i < n; i++ {
-		for j := i + 1; j <= n+i-1; j++ { 
+		for j := i + 1; j <= n+i-1; j++ {
 
 			ind := j % n
 			if nums[i] < nums[ind] {
@@ -19,4 +19,36 @@ func nextGreaterElements(nums []int) []int {
 	}
 
 	return nge
+}
+
+// optimal with stack using virtaul array
+func nextGreaterElements(nums []int) []int {
+	n := len(nums)
+	stack := []int{}
+	ans := make([]int, len(nums))
+
+	popTillNge := func(i int) {
+		for len(stack) > 0 && nums[i%n] >= stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	for i := 2*n - 1; i >= 0; i-- {
+		if i >= n {
+			popTillNge(i)
+		} else {
+			popTillNge(i)
+
+			if len(stack) == 0 {
+				ans[i] = -1
+			} else {
+				ans[i] = stack[len(stack)-1]
+			}
+		}
+
+		stack = append(stack, nums[i%n])
+
+	}
+
+	return ans
 }

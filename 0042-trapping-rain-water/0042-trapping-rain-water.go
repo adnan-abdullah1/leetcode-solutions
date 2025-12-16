304489bbdee2df92fd4c17dd032e426cf330442d
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-16 22:50:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.03%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.go
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -1,73 +1,23 @@
-// n square solution
-// func trap(arr []int) int {
-//     trapped := 0
-// 	for i := range arr {
-// 		rMax := getRightMax(arr, i)
-// 		lMax := getLeftMax(arr, i)
-// 		trapped += min(rMax, lMax) - arr[i]
-// 	}
-// 	return trapped
-// }
+func trap(height []int) int {
+	stack := []int{}
+	water := 0
 
-func getLeftMax(arr []int, i int) int {
-	maxNo := 0
-	for i := i; i >= 0; i-- {
-		if arr[i] > maxNo {
-			maxNo = arr[i]
+	for i := 0; i < len(height); i++ {
+		for len(stack) > 0 && height[i] > height[stack[len(stack)-1]] {
+			valleyInd := stack[len(stack)-1] // right side
+			stack = stack[:len(stack)-1]
+
+			if len(stack) == 0 {
+				break
+			}
+
+			w := i - stack[len(stack)-1] - 1
+
+            water += w * (min(height[i], height[stack[len(stack)-1]]) - height[valleyInd])
+
 		}
+        stack=append(stack,i)
 	}
-	return maxNo
+
+	return water
 }
-
-func getRightMax(arr []int, i int) int {
-	maxNo := 0
-	for i := i; i < len(arr); i++ {
-		if arr[i] > maxNo {
-			maxNo = arr[i]
-		}
-	}
-	return maxNo
-}
-
-// better solution can be by computing
-// premax sum, and postmax sum for every element
-// tc: o(n), sc: o(n)
-
-func trap(arr []int) int {
-	maxLeft := calcPreMax(arr)
-	maxRight := calcPostMax(arr)
-	fmt.Println(maxLeft)
-	fmt.Println(maxRight)
-
-	trapped := 0
-	for i := range arr {
-		trapped += min(maxLeft[i], maxRight[i]) - arr[i]
-	}
-	return trapped
-}
-
-func calcPreMax(arr []int) []int {
-	maxNo := 0
-	res := make([]int, len(arr))
-	for i, v := range arr {
-		if v > maxNo {
-			maxNo = v
-		}
-		res[i] = maxNo
-	}
-	return res
-}
-func calcPostMax(arr []int) []int {
-	maxNo := 0
-	res := make([]int, len(arr))
-
-	for i := len(arr) - 1; i >= 0; i-- {
-		if arr[i] > maxNo {
-			maxNo = arr[i]
-		}
-		res[i] = maxNo
-	}
-	return res
-}
-
-// most optimal soution is two pointer

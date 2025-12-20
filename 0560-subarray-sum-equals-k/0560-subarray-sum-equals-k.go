23b0922fc642e043f6a1f75920499ceffd24e902
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-20 14:36:27 +0530

    Time: 16 ms (70.56%), Space: 9.5 MB (40.04%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.go
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.go
@@ -1,19 +1,40 @@
+// func subarraySum(arr []int, k int) int {
+// 	mp := make(map[int]int)
+// 	mp[0] = 1 //keeping zero added a
+// 	s := 0
+// 	total := 0
+
+// 	for i := 0; i < len(arr); i++ {
+// 		s += arr[i]
+
+// 		// does s-k exist in map
+// 		f, v := mp[s-k]
+// 		if v {
+// 			total += f
+// 		}
+// 		mp[s]++
+
+// 	}
+// 	return total
+// }
+
 func subarraySum(arr []int, k int) int {
-	mp := make(map[int]int)
-    mp[0]=1
-	s := 0
-	total := 0
+    mp := make(map[int]int)
+    s := 0
+    total := 0
 
-	for i := 0; i < len(arr); i++ {
-		s += arr[i]
-		
-			// does s-k exist in map
-			f, v := mp[s-k]
-			if v {
-				total+=f
-			}
-		mp[s]++
+    for i := 0; i < len(arr); i++ {
+        s += arr[i]
 
-	}
-	return total
+        if s == k {
+            total++
+        }
+
+        if count, ok := mp[s-k]; ok {
+            total += count
+        }
+
+        mp[s]++
+    }
+    return total
 }

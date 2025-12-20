ca381490e58bdff19ef29f7c6484fcd6c6bf2c71
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2025-12-20 15:31:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (79.8%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.go
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.go
@@ -1,4 +1,6 @@
-func numSubarraysWithSum(arr []int, k int) int {
+// taking map with prefix sum
+
+func Approach1numSubarraysWithSum(arr []int, k int) int {
 	mp := make(map[int]int)
 	mp[0] = 1
 	s := 0
@@ -11,4 +13,39 @@ func numSubarraysWithSum(arr []int, k int) int {
 	}
 	return total
 
+}
+
+// with set decompostion
+//
+
+// taking map with prefix sum
+// count of all sub array with sum = goal
+// is equal to count of all sub array with sum <= goal - count of all sub aray with sum <= goal-1
+// then with this we are able to count all
+func numSubarraysWithSum(arr []int, k int) int {
+    return atMost(arr, k) - atMost(arr, k-1)
+}
+
+
+func atMost(arr []int, limit int) int {
+    if limit < 0 {
+        return 0
+    }
+
+    left := 0
+    sum := 0
+    count := 0
+
+    for right := 0; right < len(arr); right++ {
+        sum += arr[right]
+
+        for sum > limit {
+            sum -= arr[left]
+            left++
+        }
+
+        count += right - left + 1
+    }
+
+    return count
 }

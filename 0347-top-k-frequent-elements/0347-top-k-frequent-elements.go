ed8f493f027e418a7452bf450b37c5b04b2602af
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-01-13 23:06:01 +0530

    Time: 1 ms (66.65%), Space: 7.8 MB (80.36%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -0,0 +1,58 @@
+type Item struct {
+	no   int
+	freq int
+}
+type MaxHeap []Item
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(Item))
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	*h = old[:n-1]
+	return item
+}
+
+func (h *MaxHeap) Less(i, j int) bool {
+	return (*h)[i].freq > (*h)[j].freq
+}
+func (h *MaxHeap) Len() int {
+	return len(*h)
+}
+
+func (h *MaxHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+
+func topKFrequent(arr []int, k int) []int {
+    h := &MaxHeap{}
+	heap.Init(h)
+
+	
+	
+	mp := make(map[int]int)
+	for _, v := range arr {
+		mp[v]++
+	}
+
+	// heapify
+	for n, f := range mp {
+		heap.Push(h, Item{
+			no:   n,
+			freq: f,
+		})
+	}
+
+	ans := []int{}
+	for k > 0 && h.Len() > 0 {
+		x := heap.Pop(h).(Item)
+		ans = append(ans, x.no)
+		k--
+	}
+
+	return ans
+}

7967bf2af62a74e990fbc7c4b9526fb2237f9a7e
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-01-13 20:40:30 +0530

    Time: 0 ms (100%), Space: 4 MB (60.54%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.go
+++ 1046-last-stone-weight/1046-last-stone-weight.go
@@ -1,32 +1,49 @@
+type MaxHeap []int
+
+func (h *MaxHeap) Len() int {
+	return len(*h)
+}
+
+func (h *MaxHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j] // max heap
+}
+
+func (h *MaxHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
 func lastStoneWeight(stones []int) int {
-	inverseIntComparator := func(a, b interface{}) int {
-		return -utils.IntComparator(a, b)
-	}
-	heap := binaryheap.NewWith(inverseIntComparator)
+	h := &MaxHeap{}
+	heap.Init(h)
 
 	for _, v := range stones {
-		heap.Push(v)
+		heap.Push(h, v)
 	}
 
-	for heap.Size() > 1 {
-		n1i, _ := heap.Pop()
-		n2i, _ := heap.Pop()
+	for h.Len() > 1 {
+		n1 := heap.Pop(h).(int)
+		n2 := heap.Pop(h).(int)
 
-		n1 := n1i.(int)
-		n2 := n2i.(int)
-		if n1 == n2 {
-			continue
+		if n1 != n2 {
+			heap.Push(h, n1-n2)
 		}
-		rem := n1 - n2
-		if rem < 0 {
-			rem = -1 * rem
-		}
-		heap.Push(rem)
 	}
 
-	if heap.Size() == 0 {
+	if h.Len() == 0 {
 		return 0
 	}
-	maxi, _ := heap.Peek()
-	return maxi.(int)
+	return (*h)[0]
 }
+

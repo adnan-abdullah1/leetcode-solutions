bb1aeaa676d8c99b30cad7b40712d915d27cfbe5
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-01-17 12:57:07 +0530

    Time: 35 ms (60.73%), Space: 12.5 MB (56.16%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.go
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.go
@@ -37,7 +37,8 @@ func (h *MaxHeap) Top() any {
 	return (*h)[0]
 }
 
-func kSmallestPairs(arr1 []int, arr2 []int, k int) [][]int {
+// n*m*log(k)
+func BrutekSmallestPairs(arr1 []int, arr2 []int, k int) [][]int {
 	h := &MaxHeap{}
 	heap.Init(h)
 
@@ -70,6 +71,96 @@ func kSmallestPairs(arr1 []int, arr2 []int, k int) [][]int {
 	for h.Len() > 0 {
 		el := heap.Pop(h).(T)
 		ans = append(ans, []int{arr1[el.i], arr2[el.j]})
+	}
+
+	return ans
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+type PairSum struct {
+	Sum int
+	i   int
+	j   int
+}
+
+type MinHeap []PairSum
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(PairSum))
+}
+
+func (h *MinHeap) Pop() any {
+	d := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return d
+}
+
+func (h *MinHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *MinHeap) Less(i, j int) bool {
+	return (*h)[i].Sum < (*h)[j].Sum
+}
+
+func (h *MinHeap) Len() int {
+	return len(*h)
+}
+
+// klog(k)
+func kSmallestPairs(arr1, arr2 []int, k int) [][]int {
+
+	h := &MinHeap{}
+	heap.Init(h)
+
+	ans := [][]int{}
+
+	if len(arr1) == 0 && len(arr2) == 0 {
+		return ans
+	}
+
+	var i, j int
+	type key struct {
+		i, j int
+	}
+
+	visited := make(map[key]bool)
+
+	heap.Push(h, PairSum{
+		Sum: arr1[0] + arr2[0],
+		i:   i,
+		j:   j,
+	})
+
+	for k > 0 && h.Len() > 0 {
+		d := heap.Pop(h).(PairSum)
+		ans = append(ans, []int{arr1[d.i], arr2[d.j]})
+
+		if d.i+1 < len(arr1) {
+			if !visited[key{i: d.i + 1, j: d.j}] {
+				heap.Push(h, PairSum{
+					Sum: arr1[d.i+1] + arr2[d.j],
+					i:   d.i + 1,
+					j:   d.j,
+				})
+				visited[key{i: d.i + 1, j: d.j}] = true
+			}
+		}
+
+		if d.j+1 < len(arr2) {
+			if !visited[key{i: d.i, j: d.j + 1}] {
+				heap.Push(h, PairSum{
+					Sum: arr1[d.i] + arr2[d.j+1],
+					i:   d.i,
+					j:   d.j + 1,
+				})
+				visited[key{i: d.i, j: d.j + 1}] = true
+			}
+
+		}
+
+		k--
 	}
 
 	return ans

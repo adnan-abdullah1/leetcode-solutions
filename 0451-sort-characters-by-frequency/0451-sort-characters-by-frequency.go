edb56e45cc55cdb4c0639e8eb6da8f5717c83873
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-01-17 23:20:25 +0530

    Time: 3 ms (72.41%), Space: 7.2 MB (72.91%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.go
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.go
@@ -1,4 +1,5 @@
-func frequencySort(s string) string {
+
+func BrutefrequencySort(s string) string {
 
 	mp := make(map[int]int)
 
@@ -31,4 +32,70 @@ func frequencySort(s string) string {
 	}
 
 	return ans
+}
+
+
+type KV struct {
+	ch   int
+	freq int
+}
+type MaxHeap []KV
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, KV{
+		ch:   x.(KV).ch,
+		freq: x.(KV).freq,
+	})
+}
+
+func (h *MaxHeap) Pop() any {
+	d := (*h)[len(*h)-1]
+	*h = (*h)[0 : len(*h)-1]
+	return d
+}
+
+func (h *MaxHeap) Len() int {
+	return len(*h)
+}
+
+func (h *MaxHeap) Less(i, j int) bool {
+	return (*h)[i].freq > (*h)[j].freq
+}
+
+func (h *MaxHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+
+func frequencySort(s string) string {
+	mp := make(map[int]int)
+
+	for _, v := range s {
+		mp[int(v)]++
+	}
+
+	type Pair struct {
+		ch   int
+		freq int
+	}
+
+	h := &MaxHeap{}
+	heap.Init(h)
+
+	for k, f := range mp {
+		heap.Push(h, KV{
+			ch:   k,
+			freq: f,
+		})
+
+	}
+
+	var ans strings.Builder
+
+	for h.Len() > 0 {
+		pair := heap.Pop(h)
+		ans.WriteString(strings.Repeat(string(rune(pair.(KV).ch)), pair.(KV).freq))
+	}
+
+	return ans.String()
 }

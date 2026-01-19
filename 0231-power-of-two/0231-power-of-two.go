b408468d286ccf50f25656b82140cb05be3102fc
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-01-20 00:12:46 +0530

    Time: 0 ms (100%), Space: 4 MB (62.79%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.go
@@ -0,0 +1,24 @@
+func isPowerOfTwo(n int) bool {
+	if n == 1 {
+		return true
+	}
+
+    if n==0{
+        return false
+    }
+
+    if n%2!=0{
+        return false
+    }
+
+    for n%2==0{
+        n=n/2
+    }
+
+    if n==1{
+        return true
+    }
+
+    return false
+
+}

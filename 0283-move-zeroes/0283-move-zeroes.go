9c51b12acef6ad4effe087faecf8943b1c8f83cd
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-02-01 17:41:50 +0530

    Time: 1 ms (26.47%), Space: 8.9 MB (31.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -1,46 +1,10 @@
-// both have same complexity
-
-// func moveZeroes(nums []int) {
-//     n := len(nums)
-
-//     for i := 0; i < n; i++ {
-
-//         // if it's non-zero, just go ahead
-//         if nums[i] != 0 {
-//             continue
-//         }
-
-//         // find next non-zero
-//         j := i + 1
-//         for j < n && nums[j] == 0 {
-//             j++
-//         }
-
-//         // no more non-zeros
-//         if j == n {
-//             return
-//         }
-
-//         // swap next non-zero into current position
-//         nums[i], nums[j] = nums[j], nums[i]
-//     }
-// }
-
-// just same complexity as of above, other way of acheving same
-func moveZeroes(nums []int) {
-	n := len(nums)
-
-	pos := 0
-	for i := 0; i < n; i++ {
-
-		if nums[i] != 0 {
-			nums[pos] = nums[i]
-			pos++
+func moveZeroes(arr []int) {
+	i := 0
+	for j := 0; j < len(arr); j++ {
+		if arr[j] != 0 {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
 		}
 	}
-	// now fill zeros at rest end
-	for pos < len(nums) {
-		nums[pos] = 0
-		pos++
-	}
+
 }

0dc5dcb20298d846ef6d0159d81a6b82d27fb655
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-02-01 23:48:45 +0530

    Time: 0 ms (100%), Space: 5.9 MB (40.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,44 +1,13 @@
-
-// O(N^2)
-// func twoSum(nums []int, target int) []int {
-// 	for i := 0; i < len(nums)-1; i++ {
-// 		for j := i + 1; j < len(nums); j++ {
-// 			if nums[i]+nums[j] == target {
-// 				return []int{i, j}
-// 			}
-// 		}
-// 	}
-// 	return []int{-1, -1}
-// }
-// second solution will be sort,
-// then do target - arr[i]=key
-// then find that key with binary search
-// sorting: nlog(n)+ loop and bs (o(log(n)))
-
-// third solution
-// insert elements to map
-// then look if target-arr[i] is in map
-// this can be done in one pass also means (fill map and check in map at same time)
 func twoSum(nums []int, target int) []int {
-	mp := make(map[int]int, len(nums))
-
-	for i := 0; i < len(nums); i++ {
-		mp[nums[i]] = i
+	mp := make(map[int]int)
+	for i, v := range nums {
+		find := target - v
+		n, ok := mp[find]
+		if ok {
+			return []int{n, i}
+		} else {
+			mp[v] = i
+		}
 	}
-
-	for i := 0; i < len(nums); i++ {
-		k := target - nums[i]
-
-        v,ok:=mp[k]
-        if !ok {
-            continue
-        }
-
-        if v != i{ // took care of not returning same index ex [1,1]
-            return []int{i,v}
-        }
-        
-	}
-
 	return []int{}
 }

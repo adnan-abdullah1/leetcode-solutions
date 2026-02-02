72bc5abca6e293284ba59c8d330d531b05ef853a
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-02-02 23:19:01 +0530

    Time: 0 ms (100%), Space: 9.6 MB (87.96%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -13,23 +13,41 @@ func BrutemaxSubArray(arr []int) int {
 	return maxSum
 }
 
-func maxSubArray(arr []int) int {
+//fails for neg
+func FailmaxSubArray(arr []int) int {
 	
-
-	sum := arr[0]
-    maxSum:=arr[0]
-
-	for j := 1; j < len(arr); j++ {
-		
-		if sum+arr[j] > arr[j] {
-			sum = sum + arr[j]
-		} else{
-            sum = arr[j]
-        }
+	sum := 0
+    maxSum:=math.MinInt32
+	for j := 0; j < len(arr); j++ {
+		sum += arr[j]
+		if sum < 0 {
+			sum = 0
+		}
 
 		if sum > maxSum {
 			maxSum = sum
 		}
 	}
     return maxSum
+}
+
+
+func maxSubArray(arr []int) int {
+
+	sum := arr[0]      // best subarray ending at index 0
+	maxSum := arr[0]  // best answer so far
+
+	for j := 1; j < len(arr); j++ {
+		// this is the line you asked about
+		if sum+arr[j] > arr[j] {
+			sum = sum + arr[j]
+		} else {
+			sum = arr[j]
+		}
+
+		if sum > maxSum {
+			maxSum = sum
+		}
+	}
+	return maxSum
 }

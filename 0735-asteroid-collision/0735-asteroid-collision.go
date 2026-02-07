638093de6ff0821c97142b69c21a9f69daf18f48
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-02-07 16:26:13 +0530

    Time: 8 ms (3.92%), Space: 6.5 MB (74.84%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.go
+++ 0735-asteroid-collision/0735-asteroid-collision.go
@@ -1,37 +1,21 @@
 func asteroidCollision(arr []int) []int {
+
 	stack := []int{}
+	for _, v := range arr {
+		if v > 0 {
+			stack = append(stack, v)
+		} else {
+			for len(stack) > 0 && stack[len(stack)-1] > 0 && stack[len(stack)-1] < int(math.Abs(float64(v))) {
+				stack = stack[0 : len(stack)-1]
+			}
 
-	top := func() int {
-		return stack[len(stack)-1]
-	}
-	pop := func() {
-		if len(stack) > 0 {
-			stack = stack[:len(stack)-1]
-		}
-	}
-	for i := 0; i < len(arr); i++ {
-		isCurrAlive := true
-		curr := arr[i]
-
-		for len(stack) > 0 && curr < 0 && top() > 0 {
-			t := math.Abs(float64(top()))
-			c := math.Abs(float64(curr))
-
-			if t == c {
-				pop()
-				isCurrAlive = false
-                break
-			} else if t < c {
-				pop()
-				isCurrAlive = true
-			} else {
-				isCurrAlive = false
-                break
+			if len(stack) > 0 && stack[len(stack)-1] == int(math.Abs(float64(v))) {
+				stack = stack[0 : len(stack)-1]
+			} else if len(stack) == 0 || stack[len(stack)-1] < 0 {
+				stack = append(stack, v)
 			}
 		}
-		if isCurrAlive {
-			stack = append(stack, curr)
-		}
 	}
+
 	return stack
 }

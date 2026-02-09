dcf3431cbef4c9b04db7699c461821f9923a908e
Adnan abdullah <38535226+adnan-abdullah1@users.noreply.github.com>
2026-02-09 21:51:01 +0530

    Time: 7 ms (36.53%), Space: 8.3 MB (40.12%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.go
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.go
@@ -1,101 +1,34 @@
+func frequencySort(str string) string {
+	mp := make(map[string]int)
 
-func BrutefrequencySort(s string) string {
-
-	mp := make(map[int]int)
-
-	for _, v := range s {
-		mp[int(v)]++
+	for _, v := range str {
+		s := string(v)
+		val, _ := mp[s]
+		mp[s] = val + 1
 	}
 
 	type Pair struct {
-		ch   int
-		freq int
+		ch  string
+		cnt int
 	}
 
-	pairs := make([]Pair, 0, len(mp))
+	paris := make([]Pair, 0, len(mp))
 
 	for k, v := range mp {
-		pairs = append(pairs, Pair{
-			ch:   k,
-			freq: v,
+		paris = append(paris, Pair{
+			ch:  k,
+			cnt: v,
 		})
 	}
 
-	sort.Slice(pairs, func(i, j int) bool {
-		return pairs[i].freq > pairs[j].freq
+	sort.Slice(paris, func(i, j int) bool {
+		return paris[i].cnt > paris[j].cnt
 	})
 
 	ans := ""
-
-	for _, v := range pairs {
-		ans += strings.Repeat(string(v.ch), v.freq)
+	for _, v := range paris {
+		ans += strings.Repeat(v.ch, v.cnt)
 	}
 
 	return ans
-}
-
-
-type KV struct {
-	ch   int
-	freq int
-}
-type MaxHeap []KV
-
-func (h *MaxHeap) Push(x any) {
-	*h = append(*h, KV{
-		ch:   x.(KV).ch,
-		freq: x.(KV).freq,
-	})
-}
-
-func (h *MaxHeap) Pop() any {
-	d := (*h)[len(*h)-1]
-	*h = (*h)[0 : len(*h)-1]
-	return d
-}
-
-func (h *MaxHeap) Len() int {
-	return len(*h)
-}
-
-func (h *MaxHeap) Less(i, j int) bool {
-	return (*h)[i].freq > (*h)[j].freq
-}
-
-func (h *MaxHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-
-func frequencySort(s string) string {
-	mp := make(map[int]int)
-
-	for _, v := range s {
-		mp[int(v)]++
-	}
-
-	type Pair struct {
-		ch   int
-		freq int
-	}
-
-	h := &MaxHeap{}
-	heap.Init(h)
-
-	for k, f := range mp {
-		heap.Push(h, KV{
-			ch:   k,
-			freq: f,
-		})
-
-	}
-
-	var ans strings.Builder
-
-	for h.Len() > 0 {
-		pair := heap.Pop(h)
-		ans.WriteString(strings.Repeat(string(rune(pair.(KV).ch)), pair.(KV).freq))
-	}
-
-	return ans.String()
 }
